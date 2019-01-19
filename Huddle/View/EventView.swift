2b01b04cf6194383a39b30b78f1647d1f2b8b925
Dan Mitu <danmitu@me.com>
2019-01-18 22:30:17 -0800

    Create EventView

---
+++ Huddle/View/EventView.swift
@@ -0,0 +1,69 @@
+//
+//  EventView.swift
+//  Huddle
+//
+//  Created by Dan Mitu on 1/18/19.
+//  Copyright © 2019 Dan Mitu. All rights reserved.
+//
+
+import UIKit
+
+// Displays basic information about an event.
+// Has three lines (in order): group name, event name, date at location.
+class EventView: UIStackView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.axis = .vertical
+        addArrangedSubview(groupName)
+        addArrangedSubview(eventName)
+        addArrangedSubview(locationAndDate)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+    
+    private let groupName: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont(name: "Avenir-Black", size: 12)
+        return label
+    }()
+    
+    private let eventName: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+        return label
+    }()
+    
+    private let locationAndDate: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "HelveticaNeue-Thin", size: 18)
+        return label
+    }()
+    
+    // Helper function for maintaining consistency.
+    // Date string is relative i.e. "Today", "Yesterday", "Tomorrow"
+    // Group name is set to all caps.
+    func set(eventName: String, groupName: String, location: String, date: Date) {
+        let dateString: String
+        
+        if Calendar.current.isDateInToday(date) {
+            dateString = "Today"
+        } else if Calendar.current.isDateInYesterday(date) {
+            dateString = "Yesterday"
+        } else if Calendar.current.isDateInTomorrow(date) {
+            dateString = "Tomorrow"
+        } else {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MM/dd/yy"
+            dateString = dateFormatter.string(from: date)
+        }
+        
+        locationAndDate.text = "\(dateString) at \(location)"
+        self.groupName.text = groupName.uppercased()
+        self.eventName.text = eventName
+    }
+
+}
