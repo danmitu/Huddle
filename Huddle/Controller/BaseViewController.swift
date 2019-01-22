735b051844db6fd6a66d87601408108c9e5136cd
Dan Mitu <danmitu@me.com>
2019-01-21 18:07:31 -0800

    Add custom tab bar icons.

--- Huddle/Controller/BaseViewController.swift
+++ Huddle/Controller/BaseViewController.swift
@@ -14,29 +14,44 @@ class BaseViewController: UITabBarController {
     
     private let exploreViewController: UIViewController = {
         let vc = UIViewController()
-        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        vc.tabBarItem = UITabBarItem(title: "Explore",
+                                     image: DrawCode.imageOfExploreTabIcon(isSelected: false),
+                                     selectedImage: DrawCode.imageOfExploreTabIcon(isSelected: true))
         return vc
     }()
     
     private let calendarViewController: UIViewController = {
         let vc = UIViewController()
-        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+        vc.tabBarItem = UITabBarItem(title: "Calendar",
+                                     image: DrawCode.imageOfCalendarTabIcon(isSelected: false),
+                                     selectedImage: DrawCode.imageOfCalendarTabIcon(isSelected: true))
         return vc
     }()
     
     private let userViewController: UIViewController = {
         let vc = UIViewController()
-        vc.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 2)
+        vc.tabBarItem = UITabBarItem(title: "Profile",
+                                     image: DrawCode.imageOfUserTabIcon(isSelected: false),
+                                     selectedImage: DrawCode.imageOfUserTabIcon(isSelected: true))
         return vc
     }()
+    
+    // Colors for tab text. I wish I could put it somewhere else.
+    private let selectedColor = UIColor(red: 0.000, green: 0.478, blue: 0.980, alpha: 1.000)
+    private let unselectedColor = UIColor(red: 0.624, green: 0.624, blue: 0.624, alpha: 1.000)
     
     // MARK: - Initialization
     
     init() {
         super.init(nibName: nil, bundle: nil)
+        
+        // set the order for the view controllers, but wrap them in a navigation controller
         self.viewControllers = [exploreViewController, calendarViewController, userViewController].map {
             return UINavigationController(rootViewController: $0)
         }
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: unselectedColor], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: selectedColor], for: .selected)
     }
     
     required init?(coder aDecoder: NSCoder) {
