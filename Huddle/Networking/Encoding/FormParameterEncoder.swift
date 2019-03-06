52a846f24932f767a971fa161595fb260dcff4bf
Gerry Ashlock <gashtou4@me.com>
2019-03-05 17:45:17 -0700

    Made it a bit more readable and added mimeType to function for converting photo data to form data

--- Huddle/Networking/Encoding/FormParameterEncoder.swift
+++ Huddle/Networking/Encoding/FormParameterEncoder.swift
@@ -8,20 +8,6 @@
 //  source: https://stackoverflow.com/questions/29623187/upload-image-with-multipart-form-data-ios-in-swift
 //
 
-/* Example Request from Postman...
- 
- POST /members/profile/upload HTTP/1.1
- Host: debug1-env.m7bhnnyn8p.us-west-2.elasticbeanstalk.com
- cache-control: no-cache
- Postman-Token: bae52936-d6e3-4983-8316-c8de99ea5cdf
- Content-Type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW
- 
- Content-Disposition: form-data; name="data"; filename="/Users/danmitu/Downloads/6357600113572837231773916132_michael-scott-s-top-tantrums.png
- 
- 
- ------WebKitFormBoundary7MA4YWxkTrZu0gW--
- */
-
 import Foundation
 
 public struct FormParameterEncoder: ParameterEncoder {
@@ -30,7 +16,10 @@ public struct FormParameterEncoder: ParameterEncoder {
         
         // Create the boundary and convert the data to formdata
         let boundary = "----\(UUID().uuidString)"
-        let fullData = photoDataToFormData(data: parameters["data"] as! Data, boundary:boundary, fileName:parameters["filename"] as! String)
+        let fullData = photoDataToFormData(data: parameters["data"] as! Data,
+                                           boundary: boundary,
+                                           fileName: parameters["filename"] as! String,
+                                           mimeType: parameters["mimetype"] as! String)
         
         // Set the headers
         urlRequest.setValue("multipart/form-data; boundary=" + boundary,
@@ -42,7 +31,7 @@ public struct FormParameterEncoder: ParameterEncoder {
     }
     
     // convert the photo data to form data
-    func photoDataToFormData(data:Data,boundary:String,fileName:String) -> Data {
+    func photoDataToFormData(data:Data, boundary:String, fileName:String, mimeType:String) -> Data {
         var fullData = Data()
         
         // 1 - Start with the boundary
@@ -54,7 +43,7 @@ public struct FormParameterEncoder: ParameterEncoder {
         fullData.append(lineTwo.data(using: .utf8, allowLossyConversion: true)!)
         
         // 3 - Add the Content-Type
-        let lineThree = "Content-Type: image/jpeg\r\n\r\n"
+        let lineThree = "Content-Type: \(mimeType)\r\n\r\n"
         fullData.append(lineThree.data(using: .utf8, allowLossyConversion: true)!)
         
         // 4 - Add the actual data
@@ -64,13 +53,11 @@ public struct FormParameterEncoder: ParameterEncoder {
         let lineFive = "\r\n"
         fullData.append(lineFive.data(using: .utf8, allowLossyConversion: true)!)
         
-        // 6 - The end. make sure this is -- and then the boundary and then -- again
+        // 6 - End with the boundary (with -- at the start and end)
         let lineSix = "--" + boundary + "--\r\n"
         fullData.append(lineSix.data(using: .utf8, allowLossyConversion: true)!)
         
         return fullData
     }
-    
-    
 }
 
